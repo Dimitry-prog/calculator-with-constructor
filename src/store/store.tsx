b9d74fa4781038d35d852ac774631faf55706b96
Dimitry-prog <kfifa3@gmail.com>
2023-03-08 22:06:16 +0300

    feat: redux hooks

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -4,5 +4,7 @@ const store = configureStore({
   reducer: {},
 });
 
-export default store;
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
