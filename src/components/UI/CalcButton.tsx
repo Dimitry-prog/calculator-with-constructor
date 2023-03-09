117079184bf9084ec69fa4192b0bfcfa386789e9
Dimitry-prog <kfifa3@gmail.com>
2023-03-09 18:05:26 +0300

    feat: twMerge and constants

--- src/components/UI/CalcButton.tsx
+++ src/components/UI/CalcButton.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 type CalcButtonProps = {
   action: string;
@@ -9,8 +10,10 @@ const CalcButton: FC<CalcButtonProps> = ({ action, classes }) => {
   return (
     <button
       type="button"
-      className={`max-w-[72px] ${classes} min-w-[52px] min-h-[48px] w-full flex items-center justify-center 
-                border border-gray-200 rounded-md hover:border-blue-500 focus:bg-blue-500 focus:text-white transition-all duration-500`}
+      className={twMerge(`max-w-[72px] min-w-[52px] min-h-[48px] w-full flex items-center justify-center
+                 border border-gray-200 rounded-md hover:border-blue-500 focus:bg-blue-500 focus:text-white transition-all duration-500 ${
+                   classes ?? ``
+                 }`)}
     >
       {action}
     </button>
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const calcAction: string[] = ['/', 'x', '-', '+'];
+export const calcNumAndComma: string[] = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', ','];
